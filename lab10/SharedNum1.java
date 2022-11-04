c658889c0315b516eb4204417d6c2bcf2c6acdfa
dolnadolnadol <saiparnchollada@gmail.com>
2022-11-04 14:14:29 -0400

    lab10

---
+++ lab10/SharedNum1.java
@@ -0,0 +1,5 @@
+public class SharedNum1 {
+    private int val = 0;
+    void increment() { val++; }
+    int getVal() { return val; }
+}
---
+++ lab10/SharedNum4.java
@@ -0,0 +1,7 @@
+class SharedNum4 { // synchronized block
+    private int val = 0;
+    void increment() {
+        synchronized (this) { val++; }
+    }
+    int getVal() { return val; }
+}
